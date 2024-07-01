f6e4114a018545d7eb9bd8069c8787187524b932
aziz2790 <151596372+aziz2790@users.noreply.github.com>
2024-07-01 10:59:53 +0700

    config website

--- js/script.js
+++ js/script.js
@@ -1,1 +1,20 @@
 //ini javascript
+function displayMessage() {
+    const nama = document.getElementById('nama').value;
+    const tanggalLahir = document.getElementById('tanggalLahir').value;
+    const jenisKelamin = document.querySelector('input[name="jenisKelamin"]:checked').value;
+    const pesan = document.getElementById('pesan').value;
+
+    if (!nama || !tanggalLahir || !pesan) {
+        alert('Data masih kosong maka data belum lengkap. Mohon dilengkapi.');
+        return;
+    }
+
+    document.getElementById('displayNama').innerText = nama;
+    document.getElementById('displayTanggalLahir').innerText = tanggalLahir;
+    document.getElementById('displayJenisKelamin').innerText = jenisKelamin;
+    document.getElementById('displayPesan').innerText = pesan;
+
+    const now = new Date();
+    document.getElementById('currentTime').innerText = now.toString();
+}
